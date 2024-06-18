4ef17458a24e161e397bb2804d4d19b87025eac3
Tsuboderaaaa_c0a23052 <coa2305216@edu.teu.ac.jp>
2024-06-18 14:45:29 +0900

    画面内or画面外の判定関数実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1300, 800
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 mov_dic = {  # 移動量辞書
@@ -13,6 +13,20 @@ mov_dic = {  # 移動量辞書
     pg.K_LEFT:(-5, 0),
     pg.K_RIGHT:(+5, 0),
 }
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect　または　爆弾Rect
+    戻り値：真理値タプル（横方向、縦方向）
+    画面内ならTrue　画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -45,8 +59,15 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
