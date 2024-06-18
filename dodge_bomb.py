a2e2966b5d7d3dfc998c232f42e435cfbaa9e576
Tsuboderaaaa_c0a23052 <coa2305216@edu.teu.ac.jp>
2024-06-18 17:16:51 +0900

    演習問題1 方向切り替え(若干説明にそぐわない)

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1300, 800
+WIDTH, HEIGHT = 1800, 800
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 mov_dic = {  # 移動量辞書
@@ -27,6 +27,47 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
         tate = False
     return yoko, tate
+
+
+def kk_swicth(yoko, tate):  # こうかとんの向きを変える関数
+    """
+    引数：縦方向、横方向
+    戻り値：こうかとんsurface
+    """
+    if (yoko, tate) == (0, 0):  # 何も押していないとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+        kk_img = pg.transform.flip(kk_img, True, False)
+    elif (yoko, tate) == (+5, 0):  # 右を押しているとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+        kk_img = pg.transform.flip(kk_img, True, False)
+    elif (yoko, tate) == (+5, -5):  # 右と上を押しているとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), -45, 2.0)
+        kk_img = pg.transform.flip(kk_img, True, False)
+    elif (yoko, tate) == (0, -5):  # 上を押しているとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), -90, 2.0)
+        kk_img = pg.transform.flip(kk_img, True, False)
+    elif (yoko, tate) == (+5, +5):  # 右と下を押しているとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0)
+        kk_img = pg.transform.flip(kk_img, True, False)
+    elif (yoko, tate) == (0, +5):  # 下を押しているとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 90, 2.0)
+        kk_img = pg.transform.flip(kk_img, True, False)
+    elif (yoko, tate) == (-5, 0):  # 左を押しているとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    elif (yoko, tate) == (-5, -5):  # 左と上を押したとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), -45, 2.0)
+    elif (yoko, tate) == (-5, +5):  # 左と下を押したとき
+        kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0)
+    return kk_img   
+
+
+# def accs_tpl():  # 演習問題２途中
+#     accs = [a for a in range(1, 11)]  # 加速度のリスト
+#     bb_imgs = []
+#     for r in range(1, 11):
+#         bb_img = pg.surface((20*r, 20*r))
+#         pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+#         bb_imgs.append()
 
 
 def main():
@@ -60,6 +101,7 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+                kk_img = kk_swicth(sum_mv[0], sum_mv[1])
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
